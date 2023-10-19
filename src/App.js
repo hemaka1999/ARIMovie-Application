5fc2211e831462870b174ee5cf3f5d7c915835ee
hemaka1999 <hemaka1999@gmail.com>
2023-10-19 22:55:28 +0530

    test react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You have change counter to " + counter)
+  }, [counter]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 5)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 5)}>-</button>
+      
     </div>
   );
 }
