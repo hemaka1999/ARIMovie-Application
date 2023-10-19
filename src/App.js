f68ef3dc343cb6da92a1b49e900c19039b473701
hemaka1999 <hemaka1999@gmail.com>
2023-10-19 23:25:36 +0530

    movie app init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-
-
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You have change counter to " + counter)
-  }, [counter]);
-  
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCounter) => prevCounter + 5)}>+</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCounter) => prevCounter - 5)}>-</button>
-      
-    </div>
-  );
+import React from "react";
+const App=()=>{
+    return (
+        <h1>Hello!</h1>
+    );
 }
-
 export default App;
