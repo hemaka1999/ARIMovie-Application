2adfbda3dc991bc0a627ae0622af6c67dad91341
hemaka1999 <hemaka1999@gmail.com>
2023-10-20 17:39:40 +0530

    Movie searching ok

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import {useEffect, useState} from "react";
 import MovieCard from "./MovieCard";
 
 import './App.css';
+import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=aa80b284';
 
-const movie1 = {
-    
-        "Title": "Superman, Spiderman or Batman",
-        "Year": "2011",
-        "imdbID": "tt2084949",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
 
 const App=()=>{
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -37,13 +31,13 @@ const App=()=>{
             <div className="search">
                 <input
                 placeholder="Search for movies"
-                value={'Spiderman'}
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
-                   // src={SearchIcon}
+                    src={SearchIcon}
                     alt="search"
-                    onClick={() => {}}
+                    onClick={() => searchMovies(searchTerm)}
                 />
 
             </div>
